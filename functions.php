5fc1f4a4bbf32d3048f72778ccafd42b54638ef0
Trey Brister <webstandardcss@gmail.com>
2012-07-08 18:52:37 -0500

    HTML5 Enable Thematic

--- functions.php
+++ functions.php
@@ -247,13 +247,13 @@ add_action('thematic_belowfooter', 'foot_wrap_end', 1);
 
 add_action('wp_footer', 'footer_scripts');
 
-
+// HTML5 Enable Thematic
 //replace the standard doctype declaration and html tag opening...
 function html5_create_doctype($content) {
- $content = "<!DOCTYPE html>";
- $content .= "\n";
- $content .= "<html";
- return $content;
+	$content = "<!DOCTYPE html>";
+	$content .= "\n";
+	$content .= "<html";
+	return $content;
 }
 add_filter('thematic_create_doctype', 'html5_create_doctype');
  
@@ -266,17 +266,17 @@ add_filter('language_attributes', 'html5_language_attributes');
  
 //replace the <head> tag opening to remove the now defunct profile attribute and add the <meta charset="utf-8"> tag...
 function html5_head($content) {
- $content = "<head>";
- $content .= "\n";
- $content .= "<meta charset=\"utf-8\">";
- $content .= "\n";
- return $content;
+	$content = "<head>";
+	$content .= "\n";
+	$content .= "<meta charset=\"utf-8\">";
+	$content .= "\n";
+	return $content;
 }
 add_filter('thematic_head_profile', 'html5_head');
  
 //remove the now defunct <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> tag...
 function html5_create_charset($content) {
- $content = "";
- return $content;
+	$content = "";
+	return $content;
 }
 add_filter('thematic_create_contenttype', 'html5_create_charset');
