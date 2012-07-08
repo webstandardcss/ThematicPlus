bd9b86d92c727b97042084f2c11c5e595eb7777d
Trey Brister <webstandardcss@gmail.com>
2012-07-08 18:49:50 -0500

    Update master

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ global $imagesdir, $childtheme, $my_site, $childtheme_dir, $custom_php, $default
 $imagesdir = get_bloginfo('stylesheet_directory') . '/images';
 $childtheme = get_bloginfo('stylesheet_directory');
 $my_site = $GLOBALS[ 'blog_id' ];
-remove_filter(‘comment_text’, ‘make_clickable’, 9);
+remove_filter(ï¿½comment_textï¿½, ï¿½make_clickableï¿½, 9);
 
 
 /** 
@@ -246,3 +246,37 @@ function foot_wrap_end() { ?>
 add_action('thematic_belowfooter', 'foot_wrap_end', 1);
 
 add_action('wp_footer', 'footer_scripts');
+
+
+//replace the standard doctype declaration and html tag opening...
+function html5_create_doctype($content) {
+ $content = "<!DOCTYPE html>";
+ $content .= "\n";
+ $content .= "<html";
+ return $content;
+}
+add_filter('thematic_create_doctype', 'html5_create_doctype');
+ 
+//replace the lang attribute in the opening <html> tag...
+function html5_language_attributes($content) {
+	$content = "lang=\"en\"";
+	return $content;
+}
+add_filter('language_attributes', 'html5_language_attributes');
+ 
+//replace the <head> tag opening to remove the now defunct profile attribute and add the <meta charset="utf-8"> tag...
+function html5_head($content) {
+ $content = "<head>";
+ $content .= "\n";
+ $content .= "<meta charset=\"utf-8\">";
+ $content .= "\n";
+ return $content;
+}
+add_filter('thematic_head_profile', 'html5_head');
+ 
+//remove the now defunct <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> tag...
+function html5_create_charset($content) {
+ $content = "";
+ return $content;
+}
+add_filter('thematic_create_contenttype', 'html5_create_charset');
